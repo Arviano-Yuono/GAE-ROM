e37017b0b071f62bc492cd41413476e62f2bc712
arvi <arvianoyuono@gmail.com>
2025-03-27 13:43:25 +0700

    fix: fix encoder class

--- src/model/encoder.py
+++ src/model/encoder.py
@@ -1,19 +1,29 @@
 from model.convolution_layers import ConvolutionLayers
-from torch_geometric.nn import TopKPooling
-
+from torch_geometric.data import Data
+from torch import nn
 from src.utils import commons
 
 config = commons.get_config('configs/default.yaml')
 
-class Encoder:
-    def __init__(self, config = config['model']['encoder']):
+class Encoder(nn.Module):
+    def __init__(self, config = config):
+        super(Encoder, self).__init__()
         self.config = config
+        self.input_dim = config['model']['convolution_layers']['dim']
+        self.hidden_channels = config['model']['convolution_layers']['hidden_channels'][0]
         self.convolution_layers = ConvolutionLayers(config)
 
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-        x = self.convolution_layers(x, edge_index)
-        return x
+    def forward(self, data: Data):
+        if data.x is None:
+            raise ValueError("Input data.x is None. Please ensure data has node features.")
+            
+        for conv in self.convolution_layers.convs:
+            data.x = self.convolution_layers.act(conv(data.x, data.edge_index))
+            data.x = self.convolution_layers.dropout(data.x)
+        data.x = data.x.reshape(data.num_graphs, self.input_dim * self.hidden_channels)
+        data.x = self.convolution_layers.act(self.convolution_layers.fc1(data.x))
+        data.x = self.convolution_layers.fc2(data.x)
+        return data
     
     def reset_parameters(self):
         self.convolution_layers.reset_parameters()
