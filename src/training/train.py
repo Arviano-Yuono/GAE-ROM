e4a51b181c0834bab822fe359a3385c29ad17198
arvi <arvianoyuono@gmail.com>
2025-04-08 06:43:42 +0700

    refactor: enhance training function by modularizing optimizer and scheduler setup, adding single batch processing option, and implementing best model saving

--- src/training/train.py
+++ src/training/train.py
@@ -1,5 +1,5 @@
 from src.model.gae import GAE
-from src.utils import commons
+from src.utils.commons import get_config, save_model, load_model
 import torch
 import torch_geometric
 import torch.nn.functional as F
@@ -12,17 +12,39 @@ from torch.optim.adamw import AdamW
 
 import tqdm
 
-config = commons.get_config('configs/default.yaml')['training']
+config = get_config('configs/default.yaml')['training']
 
-def train(model, optimizer: torch.optim.Optimizer, 
+def train(model, 
           device: torch.device, 
-          scheduler: torch.optim.lr_scheduler._LRScheduler, 
           train_loader: torch_geometric.loader.DataLoader,
+          print_every = None,
+          single_batch: bool = False,
           config = config):
     
     torch.cuda.empty_cache()
     
+    # optimizer
+    if config['optimizer']['type'] == 'Adam':
+        from torch.optim.adam import Adam
+        optimizer = Adam(model.parameters(), lr=config['optimizer']['learning_rate'])
+    elif config['optimizer']['type'] == 'AdamW':
+        from torch.optim.adamw import AdamW
+        optimizer = AdamW(model.parameters(), lr=config['optimizer']['learning_rate'])
+    else:
+        raise ValueError(f"Invalid optimizer: {config['optimizer']['type']}")
+
+    # scheduler 
+    if config['scheduler']['type'] == 'StepLR':
+        from torch.optim.lr_scheduler import StepLR
+        scheduler = StepLR(optimizer, step_size=config['scheduler']['step_size'], gamma=config['scheduler']['gamma'])
+    elif config['scheduler']['type'] == 'CosineAnnealingLR':
+        from torch.optim.lr_scheduler import CosineAnnealingLR
+        scheduler = CosineAnnealingLR(optimizer, T_max=config['epochs'])
+    else:
+        raise ValueError(f"Invalid scheduler: {config['scheduler']['type']}")
+    
     train_history = dict(train_loss=[])
+    best_loss = float('inf')
 
     # training loop
     model.train()
@@ -32,26 +54,46 @@ def train(model, optimizer: torch.optim.Optimizer,
         if config['amp']:
             with torch.amp.autocast(device_type=device.type, dtype=torch.bfloat16):
                 reconstruction_loss = torch.tensor(0., device=device)
-                for batch in train_loader:
-                    # print(batch)
+                if single_batch:
+                    batch = next(iter(train_loader))
                     batch = batch.to(device)
+                    target = batch.x
                     optimizer.zero_grad()
-                    out = model(batch).to(device)
-                    reconstruction_loss += F.mse_loss(input=out, target=batch.x)
+                    out, _ = model(batch)
+                    reconstruction_loss += F.mse_loss(input=out, target=target)
+                else:
+                    for batch in train_loader:
+                        batch = batch.to(device)
+                        target = batch.x
+                        optimizer.zero_grad()
+                        out, _ = model(batch)
+                        reconstruction_loss += F.mse_loss(input=out, target=target)
         else:
             reconstruction_loss = torch.tensor(0., device=device)
-            for batch in train_loader:
-                # print(batch)
+            if single_batch:
+                batch = next(iter(train_loader))
                 batch = batch.to(device)
+                target = batch.x
                 optimizer.zero_grad()
-                out = model(batch).to(device)
-                reconstruction_loss += F.mse_loss(input=out, target=batch.x)
+                out, _ = model(batch)
+                reconstruction_loss += F.mse_loss(input=out, target=target)
+            else:
+                for batch in train_loader:
+                    batch = batch.to(device)
+                    target = batch.x
+                    optimizer.zero_grad()
+                    out, _ = model(batch)
+                    reconstruction_loss += F.mse_loss(input=out, target=target)
         loss_train = reconstruction_loss / len(train_loader)  # Average the loss
         loss_train.backward()
         optimizer.step()
         scheduler.step()
+        # save best model
+        if loss_train.item() < best_loss:
+            best_loss = loss_train.item()
+            save_model(model, f'artifacts/{config["model_name"]}_best_model.pth')
         train_history['train_loss'].append(loss_train.item())
-        if i % config['print_train'] == 0:
+        if i != 0 and i % config['print_train'] == 0:
             print(f"Epoch {i+1}/{config['epochs']}, Loss: {loss_train.item()}")
     
     return train_history
