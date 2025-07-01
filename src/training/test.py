ef11a1132ce5a1852a2766899f5488540f2aed38
arvi <arvianoyuono@gmail.com>
2025-07-01 21:56:42 +0700

    feat: add test implementation and custom metric for evaluation

--- src/training/test.py
+++ src/training/test.py
@@ -6,12 +6,15 @@ def test(model: GAE,
          test_params: torch.Tensor,
          test_loader: torch_geometric.loader.DataLoader,
          device: torch.device,
+         error_func: list[str],
          save_path: str = None,
-         save_results: bool = False):
+         save_results: bool = False,
+         mesh_path: str = "dataset/flows/configuration_1.vtu"):
     
     model = model.to(device)
     test_params = test_params.to(device)
     model.eval()
+    total_error = {f"total_{error.__name__}": 0 for error in error_func}
     test_results = []
     with torch.no_grad():
         index = 0
@@ -20,14 +23,24 @@ def test(model: GAE,
             data = data.to(device)
             latent_var = model.mapping(params)
             decoded_x = model.linear_autoencoder.decoder(latent_var)
-            data.x = model.graph_decoder(data, decoded_x)
-            test_results.append(data)
+            pred = model.graph_decoder(data, decoded_x)
+            test_results.append(pred)
             index += 1
+            for error in error_func:
+                total_error[f"total_{error.__name__}"] += error(pred, data.x).item()
+                
+    for error, value in total_error.items():
+        total_error[error] = value / (index + 1)
 
+    # save to vtu files
     if save_results:
+        mesh = pv.read(mesh_path)
+        mesh.clear_points()
+        for i, pred in enumerate(test_results):
+            pred.write_vtu(save_path + f"test_results_{i}.vtu")
         torch.save(test_results, save_path + "test_results.pt")
 
-    return test_results
+    return test_results, total_error
 
 def single_test(model: GAE,
                 test_params: torch.Tensor,
---
+++ src/utils/metrics.py
@@ -0,0 +1,26 @@
+
+import torch
+
+def mse_error(pred, target):
+    assert pred.shape == target.shape, "Prediction and target must have the same shape"
+    return torch.mean((pred - target) ** 2)
+
+def relative_error(pred, target, epsilon = 1e-4):
+    assert pred.shape == target.shape, "Prediction and target must have the same shape"
+    return torch.mean(torch.abs(pred - target) / (target + epsilon))
+
+def mae_error(pred, target):
+    assert pred.shape == target.shape, "Prediction and target must have the same shape"
+    return torch.mean(torch.abs(pred - target))
+
+def rmse_error(pred, target):
+    assert pred.shape == target.shape, "Prediction and target must have the same shape"
+    return torch.sqrt(torch.mean((pred - target) ** 2))
+
+def r2_error(pred, target):
+    assert pred.shape == target.shape, "Prediction and target must have the same shape"
+    return 1 - torch.sum((pred - target) ** 2) / torch.sum((target - torch.mean(target)) ** 2)
+
+def r2_error(pred, target):
+    assert pred.shape == target.shape, "Prediction and target must have the same shape"
+    return 0
